79adc1fa7ae10ef873280d80128c6a86478fabcc
Thomas Schardt <thosch1800@gmail.com>
2020-10-31 22:01:55 +0100

    extract AppContext

---
+++ src/LogoMqttBinding/AppContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+using LogoMqttBinding.LogoAdapter;
+using LogoMqttBinding.MqttAdapter;
+
+namespace LogoMqttBinding
+{
+  internal class AppContext : IAsyncDisposable
+  {
+    internal AppContext(ImmutableArray<Logo> logos, ImmutableArray<Mqtt> mqttClients)
+    {
+      Logos = logos;
+      MqttClients = mqttClients;
+    }
+
+    public ImmutableArray<Logo> Logos { get; }
+    public ImmutableArray<Mqtt> MqttClients { get; }
+
+    public async ValueTask DisposeAsync()
+    {
+      foreach (var logo in Logos) await logo.DisposeAsync();
+      foreach (var mqttClient in MqttClients) await mqttClient.DisposeAsync();
+    }
+  }
+}
